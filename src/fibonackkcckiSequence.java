d12adb73446a67024bee01b8a0c20902a63d39cf
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2020-03-20 18:01:51 -0700

    Create fibonackkcckiSequence.java

---
+++ src/fibonackkcckiSequence.java
@@ -0,0 +1,27 @@
+
+public class fibonackkcckiSequence {
+
+	public static void main(String[] args) {
+		int a = 0;
+		int b = 1;
+		int c = a+b;
+		System.out.println(a);
+		System.out.println(b);
+		for(int i=0; i<10; i++) {
+		System.out.println(c);
+		a = b;
+		b = c;
+		c = a+b;
+					
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
